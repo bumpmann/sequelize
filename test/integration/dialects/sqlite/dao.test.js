0e8a4a232ff105d2ad3d461dbbfbea1d97dc71fe
Arnaud Richard <arnaud@oauth.io>
2016-12-29 16:58:33 +0100

    travis test

--- test/integration/dialects/sqlite/dao.test.js
+++ test/integration/dialects/sqlite/dao.test.js
@@ -83,5 +83,13 @@ if (dialect === 'sqlite') {
 
       });
     });
+
+    describe('query that fails in a transaction', function() {
+      it('rejects its promise', function() {
+        return this.sequelize.transaction(t => {
+          return this.sequelize.query("SELECT * FROM non_existant_table", {raw: true, transaction: t}).to.eventually.be.rejected
+        }).to.eventually.be.rejected;
+      });
+    });
   });
 }
